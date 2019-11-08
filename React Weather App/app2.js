2cc9632d09ad24f4a0b3e8f15003ba04309089d4
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-11-08 13:13:08 -0800

    OWM data sucks
    need to switch to dark sky.  which means i'd need to route my search terms through another api which gives lat a lng as a function of zip or city name, bc dark sky only takes calls by lat lng...fffffff

--- React Weather App/app2.js
+++ React Weather App/app2.js
@@ -219,15 +219,35 @@ class Week extends React.Component {
 class Day extends React.Component {
    render(){
 
+      {/*Define the index from the list of 3-hour forecasts: */}
       let onceDailyIndex = 0 + this.props.number*8
-      let description = this.props.data.list[onceDailyIndex].weather[0].description
-      let time = this.props.data.list[onceDailyIndex].dt_txt
-      let icon = this.props.data.list[onceDailyIndex].weather[0].icon
+
+      {/*Some time correction terms: */}
+      let userLocationOffset = -60 * new Date().getTimezoneOffset();
+      let searchedLocationOffset = this.props.data.city.timezone;
+      let unix_timestampUTC = this.props.data.list[onceDailyIndex].dt;
+      let unix_timestampLocation = unix_timestampUTC + userLocationOffset + searchedLocationOffset;
+      let localTime = new Date( (unix_timestampUTC - userLocationOffset + searchedLocationOffset)*1000).getHours();
+
+      let description = this.props.data.list[onceDailyIndex].weather[0].description;
+      let time = this.props.data.list[onceDailyIndex].dt_txt;
+      let icon = this.props.data.list[onceDailyIndex].weather[0].icon;
       let iconPath = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-      let tempKelvin = this.props.data.list[onceDailyIndex].main.temp
+      let tempKelvin = this.props.data.list[onceDailyIndex].main.temp;
+      let maxTempKelvin = this.props.data.list[onceDailyIndex].main.temp_max
+      let minTempKelvin = this.props.data.list[onceDailyIndex].main.temp_min;
+
       let temp = {
          'C': Math.floor( kelvinToCelcius(tempKelvin) ),
          'F': Math.floor( kelvinToFahrenheit(tempKelvin) )
+      }
+      let maxTemp = {
+         'C': Math.floor( kelvinToCelcius(maxTempKelvin) ),
+         'F': Math.floor( kelvinToFahrenheit(maxTempKelvin) )
+      }
+      let minTemp = {
+         'C': Math.floor( kelvinToCelcius(minTempKelvin) ),
+         'F': Math.floor( kelvinToFahrenheit(minTempKelvin) )
       }
 
       return (
@@ -237,9 +257,11 @@ class Day extends React.Component {
             <h2>{ days[ modulus(date.getDay() + this.props.number, 7) ] }</h2>
             <h2 className="date">{ months[date.getMonth()] } { modulus( date.getDate() + this.props.number, daysInAMonth[date.getMonth()] ) }</h2>
             <img className="weatherIcon" src={iconPath} />
-            <p>Time: {time}</p>
-            <p>Temp: {temp[this.props.units]} °{this.props.units}</p>
             <p style={{'textTransform': 'capitalize'}}>{description}</p>
+            <p>Time: {localTime}</p>
+            <p className="maxTemp">{maxTemp[this.props.units]} °{this.props.units}</p>
+            <p className="minTemp">{minTemp[this.props.units]} °{this.props.units}</p>
+
          </div>
       )
    }
