45167ee044fa7237f82dc0aa2dfd16e2c516eabc
slutske22 <seth.lutske@gmail.com>
2019-11-11 22:07:37 -0800

    Update Day.js

--- src/Day.js
+++ src/Day.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 
+//----------------------------------------------------------------//
+//    Generic Use functions and terms
+//----------------------------------------------------------------//
+
 
 // Some definitions for the calendar
 function modulus(i, n){
@@ -11,6 +15,15 @@ var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
 var daysInAMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 var date = new Date();
+
+
+// from https://darksky.net/dev/docs#response-format
+// 2 good icon collections:
+// https://www.iconfinder.com/iconsets/weather-color-2
+// https://www.iconfinder.com/iconsets/the-weather-is-nice-today
+var conditions = ['clear-day', 'clear-night', 'rain', 'snow', 'sleet', 'wind', 'fog', 'cloudy', 'partly-cloudy-day', 'partly-cloudy-night']
+
+
 
 
 class Day extends React.Component {
