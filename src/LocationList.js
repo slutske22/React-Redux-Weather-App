16ac0fa5a9712ccaa24af6431d043dbc15a7fdde
Seth Lutske <52637023+slutske22@users.noreply.github.com>
2019-11-13 16:59:01 -0800

    replacced for loop with map in <LocationList>

--- src/LocationList.js
+++ src/LocationList.js
@@ -1,32 +1,28 @@
 import React from 'react';
 
 class LocationList extends React.Component {
-  constructor(props){
-     super(props)
-     this.state = {
-        listItemStyle: ''
-     }
+   constructor(props){
+      super(props)
+      this.state = {
+         listItemStyle: ''
+      }
   }
 
-  render(){
+   render(){
 
-     let locations = []
-     for (let i = 0; i < this.props.locationData.length; i++){
-        locations.push(
-           <div id={`locationResult${i}`}
-           key={i}
-           className={`location-result ${this.state.listItemStyle}`}>
-              {this.props.locationData[i].display_name}
-           </div>
-        )
-     }
+      const locations = this.props.locationData.map( (locationResult, i) => 
+         <div id={`locationResult${i}`}
+         key={i} className={`location-result ${this.state.listItemStyle}`}>
+         {locationResult.display_name}
+         </div> 
+      )
 
-     return(
-        <div className="location-list">
-           {locations}
-        </div>
-     )
-  }
+      return(
+         <div className="location-list">
+            {locations}
+         </div>
+      )
+   }
 
 }
 
