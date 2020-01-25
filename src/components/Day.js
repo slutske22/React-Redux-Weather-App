ef58d9322200af9fe8ce7a0d7281c8f660a6b28d
slutske22 <seth.lutske@gmail.com>
2020-01-25 08:27:38 -0800

    workng out some formatting and weather getting
    theres a problem with the dates though....after january 30 comes january....0?

--- src/components/Day.js
+++ src/components/Day.js
@@ -10,6 +10,13 @@ function modulus(i, n){
    return (i % n + n) % n;
 }
 
+function convertTimeStamp(timestamp){
+   let date = new Date(timestamp * 1000)
+   var hours = date.getHours() > 12 ? date.getHours() % 12 : date.getHours();
+   var ampm = date.getHours() > 12 ? ' PM' : ' AM';
+   var minutes = "0" + date.getMinutes();
+   return hours + ':' + minutes.substr(-2) + ampm;
+}
 
 
 // from https://darksky.net/dev/docs#response-format
@@ -60,12 +67,17 @@ class Day extends React.Component {
 
    render(){
 
+      console.log(this.props.weatherData)
+
       const { weatherData } = this.props
-      const hour = new Date(weatherData.daily.data[this.props.number].time * 1000).getHours()
-      const tempHi = Math.round(weatherData.daily.data[this.props.number].temperatureHigh)
-      const tempLow = Math.round(weatherData.daily.data[this.props.number].temperatureLow)
-      const summary = weatherData.daily.data[this.props.number].summary
-      const icon = weatherData.daily.data[this.props.number].icon
+      const { temperatureHigh, 
+         temperatureLow, 
+         temperatureMaxTime,
+         temperatureMinTime,
+         sunriseTime,
+         sunsetTime,
+         summary, 
+         icon } = weatherData.daily.data[this.props.number]
 
       return (
          <div className={`day ${this.state.style}`} number={this.props.number} onClick={this.expandDay} >
@@ -78,14 +90,18 @@ class Day extends React.Component {
                <img className="weatherIcon" style={{width: '100px'}}
                   src={`icons/${icon}.png`} alt={summary} title={summary} />
                {/*<p className="summary">{summary}</p>*/}
-               <p className="maxTemp">{tempHi} °F</p>
-               <p className="minTemp">{tempLow} °F</p>
+               <p className="maxTemp">{temperatureHigh.toFixed(0)} °F</p>
+               <p className="minTemp">{temperatureLow.toFixed(0)} °F</p>
             </div>
 
 
             <div className="expandedDay">
+               <img className="weatherIcon"
+                  src={`icons/${icon}.png`} alt={summary} title={summary} />
                <h2>{ daysFull[ modulus(date.getDay() + this.props.number, 7) ] }, { monthsFull[date.getMonth()] } { modulus( date.getDate() + this.props.number, daysInAMonth[date.getMonth()] ) }</h2>
                <p>{summary}</p>
+               <p>{Math.round(temperatureHigh)}°F High at {convertTimeStamp(temperatureMaxTime)}</p>
+               <p>{Math.round(temperatureLow)}°F Low at {convertTimeStamp(temperatureMinTime)}</p>
             </div>
 
          </div>
