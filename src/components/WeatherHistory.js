c7e338ca27cb1292b23df80a898bf53e9160ab6a
slutske22 <seth.lutske@gmail.com>
2020-01-29 22:00:29 -0800

    animated entry for history component...another duct tape situation

--- src/components/WeatherHistory.js
+++ src/components/WeatherHistory.js
@@ -8,16 +8,19 @@ class WeatherHistory extends React.Component {
 
    state = {
       sort: "byMonth",
-      view: "byType"
+      view: "byType",
+      class: ''
    }
 
    componentDidMount(){
       console.log('from cdm', this.props.data)
+      let delay = () => { this.setState({class: 'visible'}) };
+      setTimeout(delay, 1)
    }
 
    render() {
       return (
-         <main className="WeatherHistory">
+         <main className={`WeatherHistory ${this.state.class}`}>
             <h3>Weather Trends for {this.props.locationData[this.props.locationIndex].display_name}</h3>
             <h3>Sort by: <span>Month</span> <span>Weather Detail</span></h3>
 
