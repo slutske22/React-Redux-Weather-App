9a4856e2f667dbc5f7c8703130c8cfaae94e6981
Seth Lutske <seth.lutske@gmail.com>
2020-06-01 16:19:37 +0000

    Updates

---
+++ precache-manifest.5dfef4157cec1c1b0406925a9aecd887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48d050d0c1b08bd75d6d47ba99b2b6f",
+    "url": "/react-redux-weather-app/index.html"
+  },
+  {
+    "revision": "af88dfa9965ca77542b8",
+    "url": "/react-redux-weather-app/static/css/main.eb67e578.chunk.css"
+  },
+  {
+    "revision": "cd7adcd5bc72b4745091",
+    "url": "/react-redux-weather-app/static/js/2.402f1f6f.chunk.js"
+  },
+  {
+    "revision": "af88dfa9965ca77542b8",
+    "url": "/react-redux-weather-app/static/js/main.2843a289.chunk.js"
+  },
+  {
+    "revision": "c36a5aa6fb477cc5c3fc",
+    "url": "/react-redux-weather-app/static/js/runtime-main.99359d3f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.6c36c4d0914de60f51dfe17ae7cadb6d.js"
+  "/react-redux-weather-app/precache-manifest.5dfef4157cec1c1b0406925a9aecd887.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
